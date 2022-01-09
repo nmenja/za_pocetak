6658d40d327583b802796a1cd70b70fddac06575
nmenja <54723109+nmenja@users.noreply.github.com>
2022-01-10 00:29:39 +0100

    Ostalo je brisanje preskocene figurice
    imam neki problem sa querry selector all po dva kriterijuma koji se pozivaju sa `${}

--- sah_js/main_game.js
+++ sah_js/main_game.js
@@ -41,29 +41,43 @@ function renderBoard() {
             divPolje.addEventListener("dragover", function( event ) {
                 // prevent default to allow drop
                 event.preventDefault();
+
             }, false);
 
             divPolje.addEventListener("dragenter", function( event ) {
                 // highlight potential drop target when the draggable element enters it
-                event.target.style.background = "purple";
+                event.target.style.background = "#2b86b1"; //"purple"
+                console.log("DragEnter")
             }, false);
 
             divPolje.addEventListener("dragleave", function( event ) {
                 // reset background of potential drop target when the draggable element leaves it
                 event.target.style.background = "";
+                console.log("DragLeave")
             }, false);
 
             divPolje.addEventListener("drop", function( event ) {
                 // prevent default action (open as link for some elements)
                 event.preventDefault();
-                endI = parseInt(event.target.getAttribute("data-x"))
-                endJ = parseInt(event.target.getAttribute("data-y"))
+                endI = parseInt(event.target.getAttribute("data-x"));
+                endJ = parseInt(event.target.getAttribute("data-y"));
                 // move dragged elem to the selected drop target
                 if (ruleCheck()){
-                    event.target.style.background = "";
                     dragged.parentNode.removeChild( dragged );
                     event.target.appendChild( dragged ); 
+                    arr[startI][startJ].piece = false;
+                    let middleI = (startI + endI)/2
+                    let middleJ = (startJ + endJ)/2
+                    arr[middleI][middleJ].piece = false
+                    //OVde cu da prckam dalje :D
+                    const myNode = document.querySelectorAll('[data-x=`${middleI}`][data-y=`${middleJ}`]');
+                    console.log(`${middleI}, ${middleJ}`)
+                    // myNode.innerHTML = '';
+                    console.log(myNode.innerHTML)
                 }
+                event.target.style.background = "";
+
+                console.log("Drop");
             }, false);
             // <div data-x="0" data-y="0" data-piece="false" class="prazno"></div>
             document.getElementById("app").appendChild(divPolje);
@@ -75,13 +89,14 @@ function renderBoard() {
                     ev.preventDefault();
                     
                     
-                    // alert("radi event listener")
+                // console.log("radim Drag");
                 })
                 divFigura.addEventListener('dragstart', function(ev) {
                     startI = parseInt(ev.target.parentElement.getAttribute("data-x")) 
                     startJ = parseInt(ev.target.parentElement.getAttribute("data-y"))
                     dragged = ev.target
                     ev.dataTransfer.setData("text/plain", null);
+
                 })
                 divFigura.addEventListener('dragend', function(ev) {
                 })
@@ -92,19 +107,19 @@ function renderBoard() {
 }
 function ruleCheck(){
     // return true
-    console.log(`startI = $(startI), startJ = $(startJ), endI = $(endI), endJ = $(endJ)`)
+    console.log(`startI = ${startI}, startJ = ${startJ}, endI = ${endI}, endJ = ${endJ}`)
     // if ( Math.abs((startI + startJ) - (endI + endJ)) !== 2) break
     if ( (startI === endI) && Math.abs(startJ - endJ) == 2 && arr[endI][(startJ + endJ)/2].piece === true && arr[endI][endJ].piece === false && arr[endI][endJ].background === true) {
+        console.log("true_1")
         return true 
-        // console.log("true_1")
     }
     // else {console.log("ne moze 1")}
 
     if ( (startJ === endJ) && Math.abs(startI - endI) == 2 && arr[endJ][(startI + endI)/2].piece === true && arr[endI][endJ].piece === false && arr[endI][endJ].background === true) {
+         console.log("true_2")
          return true 
-        //  console.log("true_2")
     }
-    // else {console.log("ne moze 2")}
+    else {console.log("ne moze 2")}
     return false
     
 }
