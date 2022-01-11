b7c1438a8e22c747ceba25429f76286864ef7be8
nmenja <54723109+nmenja@users.noreply.github.com>
2022-01-11 23:04:42 +0100

    undo
    undo button

--- sah_js/main_game.js
+++ sah_js/main_game.js
@@ -1,9 +1,13 @@
-var arr = []
-var dragged
+var arr = [];
+var dragged;
 const row = 9;
 const col = 9;
 
-var startI, startJ, endI, endJ
+let stack = [];
+
+let brojTokena = 0;
+
+var startI, startJ, endI, endJ;
 
 function createBoard() {
     var arr = [];
@@ -16,10 +20,12 @@ function createBoard() {
         if (i > 5 && j < 3) value = false
         if (i > 5 && j > 5) value = false
         rowArr.push({ piece: value, background: value });
+        if (value) brojTokena++;
       }
       arr.push(rowArr);
     }
     arr[4][4] = { piece: false, background: true }
+    brojTokena--
     return arr;
 }
 function renderBoard() {
@@ -47,13 +53,11 @@ function renderBoard() {
             divPolje.addEventListener("dragenter", function( event ) {
                 // highlight potential drop target when the draggable element enters it
                 event.target.style.background = "#2b86b1"; //"purple"
-                console.log("DragEnter")
             }, false);
 
             divPolje.addEventListener("dragleave", function( event ) {
                 // reset background of potential drop target when the draggable element leaves it
                 event.target.style.background = "";
-                console.log("DragLeave")
             }, false);
 
             divPolje.addEventListener("drop", function( event ) {
@@ -68,39 +72,28 @@ function renderBoard() {
                     arr[startI][startJ].piece = false;
                     let middleI = (startI + endI)/2
                     let middleJ = (startJ + endJ)/2
-                    arr[middleI][middleJ].piece = false
+                    let middleIstr = middleI.toString()
+                    let middleJstr = middleJ.toString()
+                    arr[parseInt(middleI)][parseInt(middleJ)].piece = false
                     //OVde cu da prckam dalje :D
-                    const myNode = document.querySelectorAll('[data-x=`${middleI}`][data-y=`${middleJ}`]');
-                    console.log(`${middleI}, ${middleJ}`)
-                    // myNode.innerHTML = '';
-                    console.log(myNode.innerHTML)
+                    // querySElectorAll vraca niz, a bez all vraca single vrednost
+                    const myNode = document.querySelector(`[data-x="${middleIstr}"][data-y="${middleJstr}"]`);
+                    myNode.innerHTML = '';
+                    // console.log(myNode.innerHTML)
+                    brojTokena--
+                    const myNode2 = document.querySelector('[data-x="77"]');
+                    myNode2.innerHTML = brojTokena;
+                    arr[endI][endJ].piece = true;
+                    stack.push({startI:startI, startJ:startJ, endI:endI, endJ:endJ})
+                    console.log(stack)
                 }
                 event.target.style.background = "";
 
-                console.log("Drop");
             }, false);
             // <div data-x="0" data-y="0" data-piece="false" class="prazno"></div>
             document.getElementById("app").appendChild(divPolje);
             if (arr[i][j].piece){
-                var divFigura = document.createElement("div");
-                divFigura.classList.add("figura");
-                divFigura.setAttribute('draggable', true);
-                divFigura.addEventListener('drag', function(ev) {
-                    ev.preventDefault();
-                    
-                    
-                // console.log("radim Drag");
-                })
-                divFigura.addEventListener('dragstart', function(ev) {
-                    startI = parseInt(ev.target.parentElement.getAttribute("data-x")) 
-                    startJ = parseInt(ev.target.parentElement.getAttribute("data-y"))
-                    dragged = ev.target
-                    ev.dataTransfer.setData("text/plain", null);
-
-                })
-                divFigura.addEventListener('dragend', function(ev) {
-                })
-                divPolje.appendChild(divFigura);
+                divPolje.appendChild(createFigura());
             }
         }
     }
@@ -115,16 +108,29 @@ function ruleCheck(){
     }
     // else {console.log("ne moze 1")}
 
-    if ( (startJ === endJ) && Math.abs(startI - endI) == 2 && arr[endJ][(startI + endI)/2].piece === true && arr[endI][endJ].piece === false && arr[endI][endJ].background === true) {
+    if ( (startJ === endJ) && Math.abs(startI - endI) == 2 && arr[(startI + endI)/2][endJ].piece === true && arr[endI][endJ].piece === false && arr[endI][endJ].background === true) {
          console.log("true_2")
          return true 
     }
-    else {console.log("ne moze 2")}
+    else {console.log("ne moze")}
     return false
-    
 }
-
-
+function createFigura() {
+    var divFigura = document.createElement("div");
+    divFigura.classList.add("figura");
+    divFigura.setAttribute("draggable", true);
+    divFigura.addEventListener("drag", function (ev) {
+        ev.preventDefault();
+    });
+    divFigura.addEventListener("dragstart", function (ev) {
+        startI = parseInt(ev.target.parentElement.getAttribute("data-x"));
+        startJ = parseInt(ev.target.parentElement.getAttribute("data-y"));
+        dragged = ev.target;
+        ev.dataTransfer.setData("text/plain", null);
+    });
+    divFigura.addEventListener("dragend", function (ev) {});
+    return divFigura;
+}
 
 // 4 2 u 4 4 radi
 // startI = 4;
@@ -134,4 +140,18 @@ function ruleCheck(){
 arr = createBoard(9, 9);
 renderBoard();
 //ruleCheck();
-
+document.getElementById("undo").addEventListener('click', () => {
+    if (stack.length !== 0) {
+        let undoStep = stack.pop();
+        arr[undoStep.startI][undoStep.startJ].piece = true;
+        arr[Math.abs(undoStep.startI + undoStep.endI)/2][Math.abs(undoStep.startJ + undoStep.endJ)/2].piece = true;
+        arr[undoStep.endI][undoStep.endJ].piece = false;
+        document.querySelector(`[data-x="${undoStep.startI}"][data-y="${undoStep.startJ}"]`).appendChild(createFigura());
+        document.querySelector(`[data-x="${Math.abs(undoStep.startI + undoStep.endI)/2}"][data-y="${Math.abs(undoStep.startJ + undoStep.endJ)/2}"]`).appendChild(createFigura());
+        document.querySelector(`[data-x="${undoStep.endI}"][data-y="${undoStep.endJ}"]`).innerHTML = '';
+        brojTokena++;
+        const myNode2 = document.querySelector('[data-x="77"]');
+        myNode2.innerHTML = brojTokena;
+        console.log(undoStep);
+    }
+})
